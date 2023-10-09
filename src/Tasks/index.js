e5bb2b192e072bb8990ca000e38def688c209382
Monika Homola <monih0705@gmail.com>
2023-10-09 14:45:52 +0200

    task list without interactions yrt

---
+++ src/Tasks/index.js
@@ -0,0 +1,26 @@
+import "./style.css";
+
+const Tasks = ({ tasks, hideDoneTasks }) => (
+  <ul className="tasks">
+    {tasks.map((task) => (
+      <li
+        className={`tasks__item ${
+          task.done & hideDoneTasks ? "task__item--hidden" : " "
+        }`}
+      >
+        <button className="tasks__button tasks__button--done">
+          {task.done ? "✔" : ""}
+        </button>
+        <span
+          className={`tasks__content{
+                    task.done ? " tasks__content--done" : ""}`}
+        >
+          {task.content}
+        </span>
+        <button className="tasks__button task__button--remove">🗑️</button>
+      </li>
+    ))}
+  </ul>
+);
+
+export default Tasks;
